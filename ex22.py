0149ffd3ae9ea7d83e216393b86a1c0975331d0c
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-02-17 22:50:44 -0500

    Add files via upload

--- ex22.py
+++ ex22.py
@@ -344,3 +344,100 @@ print_two("Zed", "Shaw") # Calls the function and passes in two arguments that a
 print_two_again("Zed", "Shaw") # Calls the function and passes in two arguments that are strings
 print_one("First!") # Calls the function and passes in an argument that is a string
 print_none() # Calls the function
+
+# ex19
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print(f"You have {cheese_count} cheeses!")
+	print(f"You have {boxes_of_crackers} boxes of crackers!")
+	print("Man that's enough for a party!")
+	print("Get a blanket.\n")
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20,30)
+
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("We can even do math inside too:")
+cheese_and_crackers(10+20, 5+6)
+
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+# ex20
+
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+	print(f.read())
+
+def rewind(f):
+	f.seek(0)
+
+def print_a_line(line_count, f):
+	print(line_count, f.readline())
+
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+
+print_all(current_file)
+
+print("Now let's rewind, kind of like a tape.")
+
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+# ex21
+
+# Creates a function 'add' that takes two arguments, prints them in a formatted string, and returns the arguments added together. 
+def add(a,b):
+	print(f"ADDING {a} + {b}")
+	return a + b
+
+# Creates a function 'subtract' that takes two arguments, prints them in a formatted string, and returns the arguments and subtracts one from the other. 
+def subtract(a,b):
+	print(f"SUBTRACTING {a} - {b}")
+	return a - b
+
+# Creates a function 'multiply' that takes two arguments, prints them in a formatted string, and returns the arguments multiplied by each other. 
+def multiply(a,b):
+	print(f"MULTIPLYING {a} * {b}")
+	return a * b
+
+# Creates a function 'divide' that takes two arguments, prints them in a formatted string, and returns the arguments, one divided by the other. 
+def divide(a,b):
+	print(f"DIVIDING {a}/{b}")
+	return a / b
+
+
+print("Let's do some math with just functions!") # Prints a string
+
+age = add(30,5) # Creates a variable 'age' and assigns it the value of the function add with the values 30 and 5 passed in
+height = subtract(78,4) # Creates a variable 'height' and assigns it the value of the function subtract with the values 78 and 4 passed in
+weight = multiply(90,2) # Creates a variable 'weight' and assigns it the value of the function multiply with the values 90 and 2 passed in
+iq = divide(100,2) # Creates a variable 'iq' and assigns it the value of the function divide with the values 100 and 2 passed in
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}") # Prints a formatted string that includes the variables age, height, weight, & iq
+
+print("Here is a puzzle.") # Prints a string
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2)))) # Creates a variable and assigns it the value of divide, within multiply, within subtract, within add functions
+
+print("That become: ", what, "Can you do it by hand?") # Prints a string with the variable 'what' included
